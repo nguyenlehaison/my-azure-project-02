b35da8220e3808f0d6b892e90f2905df3993e2af
nguyenlehaison <nguyenle.haison@outlook.com>
2022-08-01 21:48:25 +0700

    Update

--- app.py
+++ app.py
@@ -2,21 +2,21 @@ from flask import Flask, request, jsonify
 from flask.logging import create_logger
 import logging
 
-# import pandas as pd
-# from sklearn.externals import joblib
-# from sklearn.preprocessing import StandardScaler
+import pandas as pd
+from sklearn.externals import joblib
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
 
-# def scale(payload):
-#     """Scales Payload"""
+def scale(payload):
+    """Scales Payload"""
 
-#     LOG.info("Scaling Payload: %s payload")
-#     scaler = StandardScaler().fit(payload)
-#     scaled_adhoc_predict = scaler.transform(payload)
-#     return scaled_adhoc_predict
+    LOG.info("Scaling Payload: %s payload")
+    scaler = StandardScaler().fit(payload)
+    scaled_adhoc_predict = scaler.transform(payload)
+    return scaled_adhoc_predict
 
 @app.route("/")
 def home():
