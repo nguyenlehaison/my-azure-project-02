94a769041cdf3061e87a0223a3ec56abeec6656e
nguyenlehaison <nguyenle.haison@outlook.com>
2022-08-02 12:16:21 +0700

    Update

--- app.py
+++ app.py
@@ -3,20 +3,20 @@ from flask.logging import create_logger
 import logging
 
 import pandas as pd
-# from sklearn.externals import joblib
-# from sklearn.preprocessing import StandardScaler
+from sklearn.externals import joblib
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
 
-# def scale(payload):
-#     """Scales Payload"""
+def scale(payload):
+    """Scales Payload"""
 
-#     LOG.info("Scaling Payload: %s payload")
-#     scaler = StandardScaler().fit(payload)
-#     scaled_adhoc_predict = scaler.transform(payload)
-#     return scaled_adhoc_predict
+    LOG.info("Scaling Payload: %s payload")
+    scaler = StandardScaler().fit(payload)
+    scaled_adhoc_predict = scaler.transform(payload)
+    return scaled_adhoc_predict
 
 @app.route("/")
 def home():
@@ -53,19 +53,19 @@ def predict():
 
     """
 
-    # try:
-    #     clf = joblib.load("boston_housing_prediction.joblib")
-    # except:
-    #     LOG.info("JSON payload: %s json_payload")
-    #     return "Model not loaded"
+    try:
+        clf = joblib.load("boston_housing_prediction.joblib")
+    except:
+        LOG.info("JSON payload: %s json_payload")
+        return "Model not loaded"
 
-    # json_payload = request.json
-    # LOG.info("JSON payload: %s json_payload")
-    # inference_payload = pd.DataFrame(json_payload)
-    # LOG.info("inference payload DataFrame: %s inference_payload")
-    # scaled_payload = scale(inference_payload)
-    # prediction = list(clf.predict(scaled_payload))
-    return jsonify({'prediction': 'prediction'})
+    json_payload = request.json
+    LOG.info("JSON payload: %s json_payload")
+    inference_payload = pd.DataFrame(json_payload)
+    LOG.info("inference payload DataFrame: %s inference_payload")
+    scaled_payload = scale(inference_payload)
+    prediction = list(clf.predict(scaled_payload))
+    return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
